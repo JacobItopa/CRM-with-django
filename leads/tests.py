a787587606a1b0c53b94bb3554bdc82f05290a10
JacobItopa <sanijacobs@gmail.com>
2022-06-22 16:45:39 +0100

    test

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,11 @@
+from audioop import reverse
 from django.test import TestCase
+from django.shortcuts import reverse
 
 # Create your tests here.
+
+class LandingPageTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse("landig-page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
