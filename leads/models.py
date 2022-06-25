41528604f8de51fba7503eea3496420e9fc30464
JacobItopa <sanijacobs@gmail.com>
2022-06-25 14:02:19 +0100

    Signals

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -28,4 +29,12 @@ class Agent(models.Model):
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.email
+
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)
     
